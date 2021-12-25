4fc410c46a21b708149d7676b15694e30a8d378c
wook3024 <wook3024@gmail.com>
2021-12-26 04:59:57 +0900

    Apply orjson in fasapi app

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import Body, FastAPI
 from celery.result import AsyncResult
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
 
 from app.worker import delay_task
 
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse)
 
 
 @app.post("/tasks", status_code=201)
-def run_task(delay: int = Body(...)) -> JSONResponse:
+def run_task(delay: int = Body(...)) -> ORJSONResponse:
     task = delay_task.delay(delay)
-    return JSONResponse(content={"id": task.id})
+    return ORJSONResponse(content={"id": task.id})
 
 
 @app.get("/tasks/{task_id}", status_code=200)
-def get_status(task_id: str) -> JSONResponse:
+def get_status(task_id: str) -> ORJSONResponse:
     task_result = AsyncResult(task_id)
     result = {"id": task_id, "status": task_result.state, "result": task_result.result}
-    return JSONResponse(content=result)
+    return ORJSONResponse(content=result)
