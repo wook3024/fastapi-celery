9fa80aad589f07e0ee67570330431875c56c0100
Shinuk Yi <wook3024@gmail.com>
2021-12-27 03:27:27 +0900

    Refactor worker structure

--- backend/worker.py
+++ backend/worker.py
@@ -1,7 +1,32 @@
+import ray
 import time
+import psutil
 
-from . import celery, ray
+from os import environ
+from celery import Celery
+
 from .utils.save import save_image_data
+
+
+num_logical_cpus = psutil.cpu_count(logical=False)
+ray.init(num_cpus=num_logical_cpus)
+
+
+celery_conf = {
+    "broker": environ.get(
+        "CELERY_BROKER_URL",
+        "redis://localhost:6379",
+    ),
+    "backend": environ.get(
+        "CELERY_RESULT_BACKEND",
+        "elasticsearch://elasticsearch:9200/background_task/result",
+    ),
+}
+celery = Celery(
+    celery_conf=__name__,
+    broker=celery_conf.get("broker"),
+    backend=celery_conf.get("backend"),
+)
 
 
 @celery.task(name="save image")
@@ -9,7 +34,9 @@ def save_image_task(contents: str, save_image_path: str, delay: int = 0) -> str:
     image_shape = save_image_data(contents, save_image_path)
     time.sleep(delay)
     return "save path: {}, image shape: {}, delay: {}s".format(
-        save_image_path, image_shape, delay
+        save_image_path,
+        image_shape,
+        delay,
     )
 
 
@@ -19,7 +46,9 @@ def default_multiprocessing_task(
     image_shape = save_image_data(contents, save_image_path)
     time.sleep(delay)
     return "save path: {}, image shape: {}, delay: {}s".format(
-        save_image_path, image_shape, delay
+        save_image_path,
+        image_shape,
+        delay,
     )
 
 
@@ -30,7 +59,9 @@ def celery_multiprocessing_task(
     image_shape = save_image_data(contents, save_image_path)
     time.sleep(delay)
     return "save path: {}, image shape: {}, delay: {}s".format(
-        save_image_path, image_shape, delay
+        save_image_path,
+        image_shape,
+        delay,
     )
 
 
@@ -41,5 +72,7 @@ def ray_multiprocessing_task(
     image_shape = save_image_data(contents, save_image_path)
     time.sleep(delay)
     return "save path: {}, image shape: {}, delay: {}s".format(
-        save_image_path, image_shape, delay
+        save_image_path,
+        image_shape,
+        delay,
     )
